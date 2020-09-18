8cadaba1e578580cf3a4b7dae9274b0e34f3f563
Kumar Gaurav <kumar.gaurav1@citrix.com>
2020-09-18 19:47:48 +0530

    - Basic Solution Added

--- src/MonotoneIncreasingDigits.kt
+++ src/MonotoneIncreasingDigits.kt
@@ -21,14 +21,39 @@ Note: N is an integer in the range [0, 10^9]
 
  **/
 fun main() {
-    val input = 322
-
-    print(MonotoneIncreasingDigits.monotoneIncreasingDigits(input))
+    val input = listOf(10, 1234, 332)
+    val answer = mutableListOf<Int>()
+    for(number in input) {
+        answer.add(MonotoneIncreasingDigits.monotoneIncreasingDigits(number))
+    }
+    print(answer)
 }
 
 object MonotoneIncreasingDigits {
     fun monotoneIncreasingDigits(N: Int): Int {
+        var currentMax = Int.MIN_VALUE
+        for (i in 1..N) {
+            if (doesNumberContainMonotoneIncreasingDigits(i) && i > currentMax) {
+                currentMax = i
+            }
+        }
 
-        return -1
+        return currentMax
+    }
+
+    private fun doesNumberContainMonotoneIncreasingDigits(N: Int): Boolean {
+        var result = true
+        val numberStr = N.toString()
+        val length = numberStr.length
+        for (i in 0 until length - 1) {
+            if (numberStr[i] <= numberStr[i + 1]) {
+                continue
+            } else {
+                result = false
+                break
+            }
+        }
+
+        return result
     }
 }
