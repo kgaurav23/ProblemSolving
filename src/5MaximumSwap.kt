ee06c01b260064e3f50f772f1f0fd8fc5ae58ecc
Kumar Gaurav <kumar.gaurav1@citrix.com>
2020-09-20 10:39:52 +0530

    - Submitted 'Accepted Solutions' for Problem 5

--- src/5MaximumSwap.kt
+++ src/5MaximumSwap.kt
@@ -23,12 +23,16 @@ Example 4:
 Input: 921456
 Output: 961452
 
+Example 5:
+Input: 1993
+Output: 9913
+
 Note: The given number is in the range [0, 108]
 
  **/
 
 fun main() {
-    val input = listOf(2736, 9973, 98368, 921456)
+    val input = listOf(2736, 9973, 98368, 921456, 1993)
     for (number in input) {
         println("Output: ${maximumSwap(number)}")
     }
@@ -36,37 +40,32 @@ fun main() {
 
 fun maximumSwap(num: Int): Int {
     val numStr = num.toString().toCharArray()
-    for (i in 1 until numStr.size) {
-        var index = i
-        while (index < numStr.size && numStr[index] <= numStr[index - 1]) {
-            index++
-        }
+    var i = 1
+    while (i < numStr.size && numStr[i] <= numStr[i - 1]) {
+        i++
+        continue
+    }
 
-        if (index == numStr.size) {
-            return num
-        }
+    if (i == numStr.size) {
+        return num
+    }
 
-        var lastIndex = i - 1
-
-        var currentMax = numStr[i]
-        var currentMaxIndex = i
-        for (j in i + 1 until numStr.size) {
-            if (numStr[j] > currentMax) {
-                currentMax = numStr[j]
-                currentMaxIndex = j
-            }
-        }
-
-        while (lastIndex > 0 && numStr[lastIndex] < currentMax) {
-            lastIndex--
-        }
-
-        if (lastIndex != -1) {
-            val temp = numStr[lastIndex]
-            numStr[lastIndex] = numStr[currentMaxIndex]
-            numStr[currentMaxIndex] = temp
+    var lastIndex = i - 1
+    var currentMax = numStr[i]
+    var currentMaxIndex = i
+    for (j in i + 1 until numStr.size) {
+        if (numStr[j] >= currentMax) {
+            currentMax = numStr[j]
+            currentMaxIndex = j
         }
     }
 
+    while (lastIndex >= 0 && numStr[lastIndex] < currentMax) {
+        lastIndex--
+    }
+
+    val temp = numStr[lastIndex + 1]
+    numStr[lastIndex + 1] = numStr[currentMaxIndex]
+    numStr[currentMaxIndex] = temp
     return numStr.joinToString("").toInt()
 }
